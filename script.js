48a5244776d40158ba7eabae7bda57a9677954d7
Jacob Hampton <jhampton792@gmail.com>
2021-02-23 11:56:10 -0600

    Changes from Lecture Added.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
 
 // Closures
-function makeFunc() {
-    let message = "Closures are cool!"
-    function displayMessage(){
-        console.log(message);
-    }
-    return displayMessage
-}
+// function makeFunc() {
+//     let message = "Closures are cool!"
+//     function displayMessage(){
+//         console.log(message);
+//     }
+//     return displayMessage
+// }
 
-const newFunc = makeFunc()
-newFunc()
+// const newFunc = makeFunc()
+// newFunc()
 
-//Closures Emulating Private methods
+// //Closures Emulating Private methods
 // const counter = function(){
 //     let privateCounter = 0
 //     function changeVal(val){
@@ -36,3 +36,54 @@ newFunc()
 // console.log(counter.value());
 
 
+// console.log("We're connected");
+
+function changeHeader(newColor) {
+    const title = document.querySelector("#title");
+    // debugger
+    // title.innerText = newTitle;
+    title.style.backgroundColor = newColor;
+}
+
+changeHeader("green");
+
+function getAllNames() {
+    let allNames = document.querySelectorAll('.pokemon');
+    // console.log(allNames);
+    for (i = 0; i < allNames.length; i++) {
+        logName(allNames[i]);
+    }
+}
+
+function logName(pokeHeader) {
+    console.log(pokeHeader.innerHTML)
+}
+
+getAllNames();
+
+function createRow(name, type, weight) {
+    let newRow = document.createElement('tr')
+
+    let newRowHead = document.createElement('th')
+        newRowHead.innerText = name;
+
+    let newTypeData = document.createElement('td')
+        newTypeData.innerHTML = type;
+
+    let newWeightData = document.createElement('td')
+        newWeightData.innerHTML= weight;
+
+    newRow.append(newRowHead, newTypeData, newWeightData);
+
+    document.getElementById('table').appendChild(newRow);
+
+    // debugger
+}
+
+createRow("Adam", "Cool", "100lbs");
+createRow("Jake", "Fire", "10000 lbs");
+createRow("Femi", "Phoenix", "10 lbs.");
+
+function deleteRow(i) {
+    document.getElementById("table").children[i].remove();
+}
